759871d49838560ab947f33897f889c154791deb
Idris <amk15534@gmail.com>
2020-01-01 19:46:04 +0200

    adding infrance engin

--- planting_app/lib/src/services/Expert_sys.dart
+++ planting_app/lib/src/services/Expert_sys.dart
@@ -1,7 +1,6 @@
 import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:planting_app/src/screens/Instructions.dart';
-import '../screens/NewDataFilling.dart';
 
 
 class UserInfo{
@@ -14,10 +13,18 @@ class UserInfo{
   int _colorScale;
   String _goal;
 
-  UserInfo(this._plant, this._plantingIvironment){}
+  UserInfo({String plant , String plantingIvironment , double plantHight , double plantWidth , String goal, String weather , int color}){
+    _plant = plant;
+    _plantingIvironment = plantingIvironment;
+    _plantHight = plantHight;
+    _plantWidth = plantWidth;
+    _colorScale = color;
+    _goal = goal;
+    _weather = weather;
+  }
 
   void setWeather(String weather){
-    _weather=weather;
+    weather=weather;
   }
  void setPlantType(String t){
     _plant=t;
@@ -32,7 +39,7 @@ class UserInfo{
     _plantStalkWidth=stalkWidth;
   }
   void setColorScale(int colorScale){
-    _colorScale=colorScale;
+    colorScale=colorScale;
   }
   void  setPlantingIvironment(String place){
     _plantingIvironment = place;
@@ -67,10 +74,35 @@ class UserInfo{
     return _goal;
   }
 
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed: () { },
+  );
+
+
   fetchInstructions(context){
     Navigator.pushReplacement(context,MaterialPageRoute(builder: (context){
       return new Instructions(plant: getPlantType(),environmnt: getPlantingIvironment(),);
     }));
+  }
+
+  advising(context){
+  
+  AlertDialog alert = AlertDialog(
+    title: Text("$_plant"),
+    content: Text("This is your results for $_plantWidth, $_colorScale "),
+    actions: [
+      okButton,
+    ],
+  );
+  
+  
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
   }
 
 }
